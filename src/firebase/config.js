18a8fea6df74e35c0255d8ad77eabbdde0a06531
maxymlyskov <raypember02@gmail.com>
2023-04-11 09:23:27 +0300

    Initialized firebase.

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwJAZBrHYFiEj3QsSN34Wj6idQ0hu42ug",
+  authDomain: "markety-38b85.firebaseapp.com",
+  projectId: "markety-38b85",
+  storageBucket: "markety-38b85.appspot.com",
+  messagingSenderId: "879847653083",
+  appId: "1:879847653083:web:f5d9974283f27b060756fe",
+};
+
+// init firebase
+
+firebase.initializeApp(firebaseConfig);
+
+// init services
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectAuth, projectFirestore, timestamp };
