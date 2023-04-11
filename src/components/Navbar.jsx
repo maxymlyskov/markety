490325f7704f29563ad7a0580347f89bc58269c0
maxymlyskov <raypember02@gmail.com>
2023-04-11 12:08:12 +0300

    Added conditional rendering to navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // styles & images
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
-import { useLogout } from "../hooks/useLogout";
 
 export default function Navbar() {
-  const { isPending, logout } = useLogout();
+  const { logout, isPending } = useLogout();
+
   return (
     <nav className="navbar">
       <ul>
@@ -22,11 +23,12 @@ export default function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          {!isPending ? (
+          {!isPending && (
             <button className="btn" onClick={logout}>
               Logout
             </button>
-          ) : (
+          )}
+          {isPending && (
             <button className="btn" disabled>
               Logging out...
             </button>
